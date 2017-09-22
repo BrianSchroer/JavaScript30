b530b59e63c242d581be639ce374363cca90ee65
Brian Schroer <brianschroer@gmail.com>
2017-09-22 07:26:04 -0500

    Added HtmlBuilder

---
+++ HtmlBuilder.js
@@ -0,0 +1,84 @@
+/**
+ * Tool for challenges where Wes uses console.log and console.table. Used to render to page instead.
+ */
+class HtmlBuilder {
+  constructor() {
+    this.html = [];
+  }
+
+  appendHtml(htmlString) {
+    this.html.push(htmlString);
+  }
+
+  appendElement(tagName, value, attributes) {
+    const adjustedAttributes = attributes ? ' ' + attributes : '';
+    this.appendHtml(`<${tagName}${adjustedAttributes}>${value}</${tagName}>`);
+  }
+
+  appendPageHeader(text) {
+    this.appendElement('h1', text);
+  }
+
+  appendSectionHeader(text) {
+    this.appendElement('h2', text);
+  }
+
+  appendParagraph(text, attributes) {
+    this.appendElement('p', text, attributes);
+  }
+
+  getPropertyNamesForArray(items) {
+    if (items && items.length) {
+      const item = items[0];
+      if (typeof item === 'object') {
+        return Object.getOwnPropertyNames(item);
+      }
+    }
+
+    return [];
+  }
+
+  appendTable(caption, items, displayCount = true) {
+    this.appendHtml('<table>');
+
+    const itemCount = items ? items.length : 0;
+    const itemCountString = displayCount ? `  (count: ${itemCount})` : '';
+    this.appendElement('caption', `${caption}${itemCountString}`);
+
+    const propertyNames = this.getPropertyNamesForArray(items);
+    const isObjectArray = propertyNames.length > 0;
+    this.appendHtml('<thead><tr>');
+    propertyNames.forEach(name => this.appendElement('th', name));
+    this.appendHtml('</tr></thead>');
+
+    this.appendHtml('<tbody>');
+    items.forEach(item => {
+      this.appendHtml('<tr>');
+      if (isObjectArray) {
+        propertyNames.forEach(name => this.appendElement('td', item[name]));
+      } else {
+        this.appendElement('td', item);
+      }
+      this.appendHtml('</tr>');
+    });
+    this.appendHtml('</tbody>');
+
+    this.appendHtml('</table>');
+  }
+
+  appendObjectHtml(caption, obj) {
+    const propertyNames = Object.getOwnPropertyNames(obj);
+
+    const props = propertyNames.map(name => ({
+      property: name,
+      value: obj[name]
+    }));
+
+    this.appendTable(caption, props, /*displayCount=*/ false);
+  }
+
+  render(selector) {
+    const elem = document.querySelector(selector);
+    elem.innerHTML = this.html.join('');
+  }
+}
